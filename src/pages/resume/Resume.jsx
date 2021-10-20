c8e82b194aef740981fa772bf8bf9a5371a828df
Anton Isaksson <anton.isak@outlook.com>
2021-10-20 10:02:36 +0200

    Resume Timeline Design

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,60 +1,56 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./resume.scss";
 
-<link
-  href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css"
-  rel="stylesheet"
-  id="bootstrap-css"
-/>;
-
 const Resume = () => {
+  const data = [
+    {
+      year: "1994",
+      happening: "Born",
+      desc: 'The year I was born, super fascinating stuff happening right here!'
+    },
+    {
+      year: "2010",
+      happening: "Born",
+      desc: 'The year I was born, super fascinating stuff happening right here!'
+    },
+    {
+      year: "2012",
+      happening: "Born",
+      desc: 'The year I was born, super fascinating stuff happening right here!'
+    },
+    {
+      year: "2014",
+      happening: "Born",
+      desc: 'The year I was born, super fascinating stuff happening right here!'
+    },
+    {
+      year: "2018",
+      happening: "Born",
+      desc: 'The year I was born, super fascinating stuff happening right here!'
+    },
+  ];
+
   return (
     <div className="container-fluid resume" id="resume">
       <h1 className="subHeader2">
         <span className="subHeaderSpan2">Resume</span>
       </h1>
 
-      <div className="row justify-content-center">
-        <div className="col-5 timelineBox">
-          <div>
-            <h2>Work Experience</h2>
-          </div>
+      <div className="row">
+        <div className="col-12 timelineContainer">
+          <div className="timeline-steps">
 
-          <div className="crockedContainer">
-            <div className="line1" />
-            <div className="line2" />
-            <div className="line3" />
-            <div className="line4" />
-            <div className="line5" />
-          </div>
-    
-          <div className="yearContainer">
-            <div className="yearBox">
-              <div className="container textContainer">
-                <h5 className="text-center">1994</h5>
+            {data.map((d) => (
+              <div className="timeline-step">
+                <div className="timeline-content">
+                  <div className="inner-circle"></div>
+                  <p className="h6 mt-3 mb-1 year">{d.year} - {d.happening}</p>
+                  
+                  <p className="mb-0 mb-lg-0 description">{d.desc}</p>
+                </div>
               </div>
-            </div>
-            <div className="happeningContainer">
-              <h3> - Graduation...</h3>
-            </div>
-          </div>
+            ))}
 
-          <div className="yearContainer2">
-            <div className="yearBox2">
-              <div className="container textContainer">
-                <h5 className="text-center">1994</h5>
-              </div>
-            </div>
-            <div className="happeningContainer2">
-              <h3> - Graduation...</h3>
-            </div>
-          </div>
-
-        </div>
-
-        <div className="col-5 timelineBox">
-          <div>
-            <h2>School</h2>
           </div>
         </div>
       </div>
