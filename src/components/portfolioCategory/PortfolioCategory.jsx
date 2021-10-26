b3ebe1c62a4db495e8340562b05a7bb940ddebc9
Anton Isaksson <anton.isak@outlook.com>
2021-10-27 00:04:26 +0200

    Deleted data

--- src/components/portfolioCategory/PortfolioCategory.jsx
+++ src/components/portfolioCategory/PortfolioCategory.jsx
@@ -1,135 +1,16 @@
 import "./portfoliocategory.scss";
 import Data from "./data/portfolioData.json";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+/* import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCss3Alt,
   faHtml5,
   faJsSquare,
   faWix,
-} from "@fortawesome/free-brands-svg-icons";
+} from "@fortawesome/free-brands-svg-icons";*/
 
 const PortfolioCard = () => {
-  const data = [
-    {
-      id: 1,
-      category: "CMS Websites",
-      projects: [
-        {
-          projectName: "Slow Down Resort",
-          img: "assets/img/slowDown.gif",
-          description:
-            "Lorem haspd awnfa asass asdsdasdasd as asf asassd asdasd.",
-          icons: [
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faWix} />,
-            },
-          ],
-          websiteName: "www.slowdownlanta.se",
-          url: "https://www.slowdownlanta.se/",
-        },
-        {
-          projectName: "Lilla Svenska Skolan",
-          img: "assets/img/svenskaSkolan.png",
-          description:
-            "Lorem haspd awnfa asass asdsdasdasd as asf asassd asdasd.",
-          icons: [
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faWix} />,
-            },
-          ],
-          websiteName: "www.svenskaskolanlanta.se",
-          url: "http://www.svenskaskolanlanta.se/",
-        },
-        {
-          projectName: "Slow Down Beach",
-          img: "assets/img/slowDownBeachvolley.gif",
-          description:
-            "Lorem haspd awnfa asass asdsdasdasd as asf asassd asdasd.",
-          icons: [
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faWix} />,
-            },
-          ],
-          websiteName: "www.slowdownbeachvolley.se",
-          url: "https://www.slowdownbeachvolley.se/",
-        },
-      ],
-    },
 
-    {
-      id: 2,
-      category: "Front End",
-      projects: [
-        {
-          projectName: "Halloween Memory",
-          img: "assets/img/halloweenMemory.gif",
-          description:
-            "My first ever Javascript project! In 2019, about 3 months in to Code Academys Full Stack Program, the mission was to make an application using only HTML, CSS and JS(jQuery). This is the result!",
-          icons: [
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faCss3Alt} />,
-            },
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faHtml5} />,
-            },
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faJsSquare} />,
-            },
-          ],
-          websiteName: "GithubPages/mystery-memory",
-          url: "https://antonisaksson.github.io/mystery-memory/",
-        },
-        {
-          projectName: "React Project 2",
-          img: "",
-          description:
-            "Lorem haspd awnfa asass asdsdasdasd as asf asassd asdasd.",
-          icons: [
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faWix} />,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 3,
-      category: "Full Stack",
-      projects: [
-        {
-          projectName: "Game Design 2d",
-          img: "assets/img/gameDesign.gif",
-          description:
-            "Lorem haspd awnfa asass asdsdasdasd as asf asassd asdasd.",
-          websiteName: "www.gamedesign-2d.com",
-          url: "https://gamedesign-2d.herokuapp.com/",
-          icons: [
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faCss3Alt} />,
-            },
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faHtml5} />,
-            },
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faJsSquare} />,
-            },
-          ],
-        },
-        {
-          projectName: "Python Project 2",
-          img: "",
-          description:
-            "Lorem haspd awnfa asass asdsdasdasd as asf asassd asdasd.",
-          icons: [
-            {
-              icon: <FontAwesomeIcon className="m-2" icon={faWix} />,
-            },
-          ],
-        },
-      ],
-    },
-  ];
 
   return (
     <>
